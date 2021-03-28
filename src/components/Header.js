9e3b8ff8442a2225c065e071d884c4596c98d99f
Christine Fong <christinefongwl@gmail.com>
2021-03-28 20:12:53 +0800

    modified font style of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <nav className='nav-wrapper blue-grey'>
       <div className='container'>
-        <a href='#' className='brand-logo center'>COVID-19 DASHBOARD</a>
+        <a href='#' className='header-text'>COVID-19 DASHBOARD</a>
       </div>
     </nav>
   );
