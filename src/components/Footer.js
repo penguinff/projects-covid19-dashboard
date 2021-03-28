50b4c76a6bceba38d59d445b3353ceb7087a3c60
Christine Fong <christinefongwl@gmail.com>
2021-03-29 00:29:38 +0800

    changed footer styling and added author info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,9 @@
 const Footer = () => {
   return (
-    <footer className='page-footer blue-grey'>
-      <div className='footer-copyright'>
-        <div className='container'>
-          Copyright © 2021 by Christine Fong
-          <a className='grey-text text-lighten-4 right' href='https://disease.sh/' target='_blank'>Data source: https://disease.sh/</a>
-        </div>
-      </div>
+    <footer className='footer page-footer blue-grey'>
+      <span>Copyright © 2021 by Christine Fong</span>
+      <span>練習作品 | 作者：Chirstine Fong | <a className='grey-text text-lighten-4' href='https://github.com/penguinff' target='_blank' rel='noreferrer noopener'>Github</a></span>
+      <span>資料來源：<a className='grey-text text-lighten-4' href='https://disease.sh/' target='_blank' rel='noreferrer noopener'>https://disease.sh/</a></span>
     </footer>
   )
 }
