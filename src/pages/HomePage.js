89998aca2cf6a6d7a24d0a6bb47029e85e0e0002
Christine Fong <christinefongwl@gmail.com>
2021-03-28 21:51:39 +0800

    removed NewsCards from HomePage & push components to center of the page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,22 @@
 import InfoCards from '../components/InfoCards';
 import MapChartGroup from '../components/MapChartGroup';
-import NewsCards from '../components/NewsCards';
+// import NewsCards from '../components/NewsCards';
 
 const HomePage = () => {
   return (
     <div className='section row'>
-      <div className='col s12 m12 l2'>
+      <div className='col s12 m12 l2 push-l1'>
         <InfoCards />
       </div>
-      <div className='col s12 m12 l8'>
+      <div className='col s12 m12 l8 push-l1'>
         <MapChartGroup />
-      </div>
-      <div className='col s12 m12 l2'>
-        <NewsCards />
       </div>
     </div>
   );
 }
 
 export default HomePage;
+
+// <div className='col s12 m12 l2'>
+//   <NewsCards />
+// </div>
