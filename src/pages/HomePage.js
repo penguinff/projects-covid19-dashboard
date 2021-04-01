8ebf0457196bf5702b219191245ce8d74976d4a4
Christine Fong <christinefongwl@gmail.com>
2021-04-01 23:21:25 +0800

    adjusted responsive layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,10 +4,10 @@ import MapChartGroup from '../components/MapChartGroup';
 const HomePage = () => {
   return (
     <div className='section row'>
-      <div className='col s12 m12 l2 push-l1'>
+      <div className='col s12 m3 xl2 push-xl1'>
         <InfoCards />
       </div>
-      <div className='col s12 m12 l8 push-l1'>
+      <div className='col s12 m9 xl8 push-xl1'>
         <MapChartGroup />
       </div>
     </div>
