5614d92ca11c47c7ee54fcf27192e7ad1621cd5f
Christine Fong <christinefongwl@gmail.com>
2024-05-30 17:20:33 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='footer page-footer blue-grey'>
-      <span>Copyright © 2021 by Christine Fong</span>
+      <span>Copyright © 2024 by Christine Fong</span>
       <span>Developed by : Chirstine Fong | <a className='grey-text text-lighten-4' href='https://github.com/penguinff' target='_blank' rel='noreferrer noopener'>Github</a></span>
       <span>Data Source : <a className='grey-text text-lighten-4' href='https://disease.sh/' target='_blank' rel='noreferrer noopener'>https://disease.sh/</a></span>
     </footer>
