4c674dd479bfb94f9605817ef3d61e18812e554f
Christine Fong <christinefongwl@gmail.com>
2024-05-30 17:09:16 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ const Footer = () => {
   return (
     <footer className='footer page-footer blue-grey'>
       <span>Copyright © 2021 by Christine Fong</span>
-      <span>練習作品 | 作者：Chirstine Fong | <a className='grey-text text-lighten-4' href='https://github.com/penguinff' target='_blank' rel='noreferrer noopener'>Github</a></span>
-      <span>資料來源：<a className='grey-text text-lighten-4' href='https://disease.sh/' target='_blank' rel='noreferrer noopener'>https://disease.sh/</a></span>
+      <span>Developed by : Chirstine Fong | <a className='grey-text text-lighten-4' href='https://github.com/penguinff' target='_blank' rel='noreferrer noopener'>Github</a></span>
+      <span>Data Source : <a className='grey-text text-lighten-4' href='https://disease.sh/' target='_blank' rel='noreferrer noopener'>https://disease.sh/</a></span>
     </footer>
   )
 }
